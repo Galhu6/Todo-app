ec550909fc620f34e4342103f30da8d12ea05f67
= <=>
2025-06-22 23:59:09 +0300

    wrappped the auth

--- src/screens/AuthScreen/AuthScreen.tsx
+++ src/screens/AuthScreen/AuthScreen.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react"
-import { GoogleLoginButton } from "../../components/Auth/GoogleLogin/GoogleLogin"
 import { AuthLogin } from "../../components/Auth/AuthLogin/AuthLogin";
 
 export const AuthScreen = () => {
 
     return (
-        <></>
+        <div className="auth-screen">
+            <AuthLogin />
+
+        </div>
     )
 }
